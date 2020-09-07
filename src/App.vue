01601e0240078d905f4cbc208a5adf327c85ab83
jiancl <1424371827@qq.com>
2020-09-07 10:31:33 +0800

    goods menu

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
+/* eslint-disable vue/no-unused-components */
 <template>
   <div id="app"  @touchmove.prevent>
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
 import { getSeller } from 'api'
 import VHeader from 'components/v-header/v-header'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
+import Tab from 'components/tab/tab'
 
 export default {
   name: 'App',
   data () {
     return {
       seller: {}
+    }
+  },
+  computed: {
+    tabs () {
+      return [
+        {
+          label: '商品',
+          component: Goods,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '评论',
+          component: Ratings,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '商家',
+          component: Seller,
+          data: {
+            seller: this.seller
+          }
+        }
+      ]
     }
   },
   created () {
@@ -26,6 +61,7 @@ export default {
     }
   },
   components: {
+    Tab,
     VHeader
   }
 }
